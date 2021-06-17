fb8da291fe5adb6f70a1c895bc68c9cded980fc7
Tiberius88 <85104944+Tiberius88@users.noreply.github.com>
2021-06-17 14:10:18 +0200

    Create Bundeslaender.kt

---
+++ app/src/main/java/com/example/mathe_spiel_rev02/Bundeslaender.kt
@@ -0,0 +1,11 @@
+package com.example.mathe_spiel_rev02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Bundeslaender : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bundeslaender)
+    }
+}
