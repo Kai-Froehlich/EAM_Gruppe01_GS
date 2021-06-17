519ba176b4be7241984cb2c980158ea4aa28a36e
Tiberius88 <85104944+Tiberius88@users.noreply.github.com>
2021-06-17 14:10:28 +0200

    Create Hauptstaedte_der_Nachbarlaender.kt

---
+++ app/src/main/java/com/example/mathe_spiel_rev02/Hauptstaedte_der_Nachbarlaender.kt
@@ -0,0 +1,11 @@
+package com.example.mathe_spiel_rev02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Hauptstaedte_der_Nachbarlaender : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hauptstaedte_der_nachbarlaender)
+    }
+}
