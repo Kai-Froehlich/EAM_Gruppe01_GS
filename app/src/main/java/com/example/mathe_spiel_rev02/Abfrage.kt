bac079972725a9f0220ee065bb4944f29f127279
Tiberius88 <85104944+Tiberius88@users.noreply.github.com>
2021-06-17 14:12:30 +0200

    Update Abfrage.kt

--- app/src/main/java/com/example/mathe_spiel_rev02/Abfrage.kt
+++ app/src/main/java/com/example/mathe_spiel_rev02/Abfrage.kt
@@ -1,11 +1,18 @@
 package com.example.mathe_spiel_rev02
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.aldebaran.qi.sdk.QiContext
 import com.aldebaran.qi.sdk.QiSDK
 import com.aldebaran.qi.sdk.RobotLifecycleCallbacks
+import com.aldebaran.qi.sdk.`object`.actuation.Animate
+import com.aldebaran.qi.sdk.`object`.conversation.Chat
+import com.aldebaran.qi.sdk.`object`.conversation.QiChatbot
+import com.aldebaran.qi.sdk.`object`.conversation.Topic
 import com.aldebaran.qi.sdk.`object`.locale.Language
+import com.aldebaran.qi.sdk.`object`.locale.Locale
 import com.aldebaran.qi.sdk.`object`.locale.Region
 import com.aldebaran.qi.sdk.builder.ChatBuilder
 import com.aldebaran.qi.sdk.builder.QiChatbotBuilder
@@ -13,17 +20,58 @@ import com.aldebaran.qi.sdk.builder.TopicBuilder
 import com.aldebaran.qi.sdk.design.activity.RobotActivity
 
 class Abfrage : RobotActivity(), RobotLifecycleCallbacks {
+//Beginn 1 neu
+    //Store the qiChatbot
+    lateinit var qiChatbot: QiChatbot
+
+    lateinit var chat: Chat
+    lateinit var topic: Topic
+    lateinit var locale: Locale
+
+    //anlegen der 4 auswahlbutton
+    lateinit var button1: Button
+    lateinit var button2: Button
+    lateinit var button3: Button
+    lateinit var button4: Button
+
+    //Store the animate action
+    private var animate: Animate? = null
+
+//Ende 1 neu
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_abfrage)
-
+//Beginn 2 neu
+        button1 = findViewById(R.id.Bundeslaender)
+        button2 = findViewById(R.id.Landeshauptstaedte)
+        button3 = findViewById(R.id.Nachbarlaender)
+        button4 = findViewById(R.id.Hauptstaedte_der_Nachbarlaender)
+//Ende 2 neu
         QiSDK.register(this,this)
+//Beginn 3 neu
+        button1.setOnClickListener {
+            val intent = Intent(this,Bundeslaender::class.java)
+            startActivity(intent)
+        }
+        button2.setOnClickListener {
+            val intent = Intent(this,Landeshauptstaedte::class.java)
+            startActivity(intent)
+        }
+        button3.setOnClickListener {
+            val intent = Intent(this,Nachbarlaender::class.java)
+            startActivity(intent)
+        }
+        button4.setOnClickListener {
+            val intent = Intent(this,Hauptstaedte_der_Nachbarlaender::class.java)
+            startActivity(intent)
+        }
+//Ende 3 neu
     }
 
     override fun onRobotFocusGained(qiContext: QiContext?) {
         //Festlegen der Sprache des Roboters//
         val locale = com.aldebaran.qi.sdk.`object`.locale.Locale(Language.GERMAN, Region.GERMANY)
-        //Einbinden des zuvor erstellten Topics
+        //Einbinden des erstellten Topics
         val topic = TopicBuilder.with(qiContext).withResource(R.raw.abfrage_laender_und_hauptstaedte).build()
         //Erstellen eines qiChatbots mittels Chatbotsbuilder und unter Einbeziehung der Spracheinstellungen
         val qiChatbot = QiChatbotBuilder.with(qiContext).withTopic(topic).withLocale(locale).build()
