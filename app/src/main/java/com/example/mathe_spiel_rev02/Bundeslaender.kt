f0b12136943d07de8e4c21a0f4913ce98d273a4f
Tiberius88 <85104944+Tiberius88@users.noreply.github.com>
2021-07-04 11:04:07 +0200

    Update Bundeslaender.kt

--- app/src/main/java/com/example/mathe_spiel_rev02/Bundeslaender.kt
+++ app/src/main/java/com/example/mathe_spiel_rev02/Bundeslaender.kt
@@ -1,11 +1,114 @@
 package com.example.mathe_spiel_rev02
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import com.aldebaran.qi.sdk.QiContext
+import com.aldebaran.qi.sdk.QiSDK
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks
+import com.aldebaran.qi.sdk.`object`.actuation.Animate
+import com.aldebaran.qi.sdk.`object`.actuation.Animation
+import com.aldebaran.qi.sdk.`object`.conversation.*
+import com.aldebaran.qi.sdk.`object`.locale.Language
+import com.aldebaran.qi.sdk.`object`.locale.Locale
+import com.aldebaran.qi.sdk.`object`.locale.Region
+import com.aldebaran.qi.sdk.builder.*
+import com.aldebaran.qi.sdk.design.activity.RobotActivity
 
-class Bundeslaender : AppCompatActivity() {
+class Bundeslaender : RobotActivity(), RobotLifecycleCallbacks {
+
+    // Store the QiChatbot.
+    lateinit var qiChatbot: QiChatbot
+    lateinit var chat: Chat
+    lateinit var topic : Topic
+    lateinit var locale : Locale
+
+    lateinit var button1: Button
+
+    // Store the Animate action.
+    private var animate: Animate? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bundeslaender)
+
+        button1 = findViewById(R.id.home1)
+
+        QiSDK.register(this,this)
+
+        button1.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    class MyQiChatExecutor(qiContext: QiContext?): BaseQiChatExecutor(qiContext){
+        override fun runWith(params: MutableList<String>?) {
+            animate(qiContext)
+        }
+
+        override fun stop() {
+            TODO("Not yet implemented")
+        }
+        private fun animate(qiContext: QiContext?){
+            val animation: Animation = AnimationBuilder.with(qiContext).withResources(R.raw.clapping_b001).build()
+            val animate: Animate = AnimateBuilder.with(qiContext).withAnimation(animation).build()
+            animate.run()
+        }
+    }
+
+
+    override fun onRobotFocusGained(qiContext: QiContext?) {
+
+
+        // Festlegen der Sprache des Roboters
+        locale = com.aldebaran.qi.sdk.`object`.locale.Locale(Language.GERMAN, Region.GERMANY)
+        // Einbinden des zuvor erstellen Topics
+        topic = TopicBuilder.with(qiContext).withResource(R.raw.bundeslaender).build()
+        // Erstellen qiChatbot
+        qiChatbot = QiChatbotBuilder.with(qiContext).withTopic(topic).withLocale(locale).build()
+        // Chat erstellen
+        chat = ChatBuilder.with(qiContext).withChatbot(qiChatbot).withLocale(locale).build()
+
+        randomize()
+        chat.addOnStartedListener { goToBookmark("START") }
+
+
+        // Animation
+        val executors = HashMap<String,QiChatExecutor>()
+        executors["Applaus"] = MyQiChatExecutor(qiContext)
+        qiChatbot.executors = executors
+        val chatbots = mutableListOf<Chatbot>()
+        chatbots.add(qiChatbot)
+
+
+        chat.async().run()
+    }
+
+    private fun  goToBookmark(bookmarkName : String) {
+        qiChatbot.goToBookmark(
+            topic.bookmarks[bookmarkName],
+            AutonomousReactionImportance.HIGH,
+            AutonomousReactionValidity.IMMEDIATE)
+    }
+
+    private fun randomize() {
+        //val Zahl_A = Random.nextInt(0,20)
+        //qiChatbot.variable("peppersNumber").value = "$Zahl_A"
+
+    }
+
+    override fun onRobotFocusLost() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRobotFocusRefused(reason: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        QiSDK.unregister(this,this)
     }
 }
